934a9733b5ded2cbd7a906de5b8cebb438150532
Kieran Chambers <kieran.chambers@icloud.com>
2024-02-01 13:27:00 +0000

    Comment code

--- run.py
+++ run.py
@@ -44,9 +44,12 @@ def create_task():
     Asks user to input their task and appends it to Tasks worksheet
     """
     task = input('\nEnter your todo: ')
+    # Assigning tasks worksheet to task_sheet variable
     task_list = SHEET.worksheet('Tasks')
+    # Appending the task to the worksheet row
     task_list.append_row([task])
-    print("")
+    print('\nAdding your new task...')
+    # Showing the updated list to the user
     display_list()
 
 
@@ -56,22 +59,32 @@ def task_done():
     """
     # Refactor? Move list to global scope with list in create_task function?
     task_list = SHEET.worksheet('Tasks')
+    # Assigning Done worksheet to done_list var
     done_list = SHEET.worksheet('Done')
+    # Shows users tasks on their list
     display_list()
+    # Asks user which task they have completed
+    # This gets assigned to find_task
     find_task = input("Which task have you completed? ")
 
     try:
-        # Looks for cell with matching value
+        # Looks for cell with matching value to find_task,
+        # Assign this to the done_task var
         done_task = task_list.find(find_task)
         # Adds the completed task to the Done sheet
         done_list.append_row([done_task.value])
         # Need to delete task from cell and shift all cells up one here...
+
+    # If no matching task is found, user given choice to try again or return to menu
     except (TypeError, AttributeError) as e:
         print(f'''\nNo task found matching {find_task}... Please try again, \
 or enter the word menu to return to the main menu.\n''')
+        # Convert string to lower in case caps lock is enabled
         if find_task.lower() == 'menu':
+            # Return user to the menu
             user_options()
         else:
+            # Continue to the task_done function again
             task_done()
 
 
@@ -83,8 +96,8 @@ def user_options():
     choice = input('''\nWhat would you like to do?\n
 1: Create a new task\n2: View your to do list
 3: Mark a task as done
-4: Edit a task\nType 'exit' to quit: ''')
-
+4: Edit a task\nOr type 'exit' to quit: ''')
+    # Checking user input against the listed options
     if choice == str(1):
         print("")
         create_task()
@@ -103,6 +116,8 @@ def user_options():
         user_options()
     elif choice.lower() == 'exit':
         exit()
+    # Tell user if their choice is not valid
+    # and runs the options menu again
     else:
         print('\nInvalid selection, please try again.\n')
         user_options()
