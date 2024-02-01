da611fe3113d5d5e5dc69828fc99c85f2493a55b
Kieran Chambers <kieran.chambers@icloud.com>
2024-02-01 11:01:55 +0000

    Create user_options function to take user input and run corresponding function on page load

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+import os # use os.system('clear') to clean up output
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -11,15 +12,22 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('todo-list-app')
-    
+
 
 def app_load():
+    """
+    Prints welcome message to user on app load and runs user_options function
+    """
     print('Welcome to your to-do list')
     print("")
-    display_list()
+    user_options()
 
 
 def display_list():
+    """
+    Checks if to do list is empty, if so, asks user to add a task
+    if not empty, prints tasks
+    """
     todo_list = SHEET.worksheet('Tasks').get_all_values()
     if todo_list == []:
         print('Your to do list is empty! Add a task.')
@@ -30,11 +38,35 @@ def display_list():
 
 
 def create_task():
+    """
+    Asks user to input their task and appends it to Tasks worksheet
+    """
     task = input('\nEnter your todo: ')
     list = SHEET.worksheet('Tasks')
     list.append_row([task])
     print("")
     display_list()
+
+
+def user_options():
+    """
+    Uses if/else to take user input and run corresponding function
+    """
+    choice = input("\nWhat would you like to do?\n1: Create a new task\n2: View your to do list\n3: Edit a task\nor enter 0 to quit: ")
+    if choice == str(1):
+        create_task()
+        user_options()
+    elif choice == str(2):
+        display_list()
+        user_options()
+    elif choice == str(3):
+        print('I need to make function to edit task...')
+        user_options()
+    elif choice == str(0):
+        exit()
+    else:
+        print('\nInvalid selection, please try again.\n')
+        user_options()
 
 
 def main():
