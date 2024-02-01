ec71c0bf93f14a3ff4cad5c974b7a04672a6c4ce
Kieran Chambers <kieran.chambers@icloud.com>
2024-02-01 10:28:13 +0000

    Write functions to add tasks to list and display to do items

--- run.py
+++ run.py
@@ -11,3 +11,31 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('todo-list-app')
+    
+
+def app_load():
+    print('Welcome to your to-do list')
+    print("")
+    display_list()
+
+
+def display_list():
+    print('Here are your tasks to complete:')
+    todo_list = SHEET.worksheet('Tasks').get_all_values()
+    print(todo_list)
+
+
+def create_task():
+    task = input('\nEnter your todo: ')
+    list = SHEET.worksheet('Tasks')
+    list.append_row([task])
+    print("")
+    display_list()
+
+
+def main():
+    app_load()
+
+
+main()
+create_task()
